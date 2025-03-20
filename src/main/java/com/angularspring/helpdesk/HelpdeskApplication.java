b0d4b44fcaff99f441d6b031a7af683ce91e81ab
Pedro Karasudani <pedro.karasudani@hotmail.com>
2025-03-20 13:01:24 -0300

    mudando o properties de dev, para update

--- src/main/java/com/angularspring/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/angularspring/helpdesk/HelpdeskApplication.java
@@ -1,22 +1,9 @@
 package com.angularspring.helpdesk;
 
-import com.angularspring.helpdesk.domain.Chamado;
-import com.angularspring.helpdesk.domain.Cliente;
-import com.angularspring.helpdesk.domain.Tecnico;
-import com.angularspring.helpdesk.domain.enums.Perfil;
-import com.angularspring.helpdesk.domain.enums.Prioridade;
-import com.angularspring.helpdesk.domain.enums.Status;
-import com.angularspring.helpdesk.repositories.ChamadoRepository;
-import com.angularspring.helpdesk.repositories.ClienteRepository;
-import com.angularspring.helpdesk.repositories.PessoaRepository;
-import com.angularspring.helpdesk.repositories.TecnicoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-import java.util.TreeMap;
 
 @SpringBootApplication
 public class HelpdeskApplication {
