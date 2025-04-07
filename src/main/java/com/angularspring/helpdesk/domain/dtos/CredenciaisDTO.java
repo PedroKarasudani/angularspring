e4d1951429be58ff442ddc405a4e4312d5092cbd
Pedro Karasudani Desktop <pedro.karasudani@hotmail.com>
2025-04-06 21:56:38 -0300

    JWT: Criando classe UserDetailsServiceImpl conforme contrato SpringSecurity e criando classe CredencialDTO e configurando JWT no application.properties

---
+++ src/main/java/com/angularspring/helpdesk/domain/dtos/CredenciaisDTO.java
@@ -0,0 +1,23 @@
+package com.angularspring.helpdesk.domain.dtos;
+
+public class CredenciaisDTO {
+
+    private String email;
+    private String senha;
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
